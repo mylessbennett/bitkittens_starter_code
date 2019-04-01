259c5543c28b8361a1a90aa9df3e1c376f42e64a
Myles Bennett <myles.bennett@hotmail.com>
2019-04-01 15:10:13 -0400

    set up event handler on summon cats button

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const summonKitties = document.querySelector('.summon-cats');
+    document.addEventListener('click', function() {
+        console.log('clicked');
+    })
 });
