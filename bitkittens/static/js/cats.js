ea7c49616e0af0b2670a5130e2013f4cafb081cf
Myles Bennett <myles.bennett@hotmail.com>
2019-04-01 16:05:20 -0400

    added cat images to the DOM/completed exercise

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const summonKitties = document.querySelector('.summon-cats');
-    document.addEventListener('click', function() {
-        console.log('clicked');
+    summonKitties.addEventListener('click', function() {
+        axios.get('http://bitkittens.herokuapp.com/cats.json')
+        .then(function(response) {
+            const catBoxes = document.querySelectorAll('.cat-box')
+            catBoxes.forEach((catBox, i) => {
+                const catImage = document.createElement('img');
+                catImage.src = response.data.cats[i].photo
+                catImage.alt = response.data.cats[i].name
+                catBox.appendChild(catImage);
+            });
+        })
     })
 });
